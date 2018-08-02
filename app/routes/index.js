74e060a68108276faef01a03d2b03d519efe0d8d
CyberGX <anonymous.aquila@gmail.com>
2018-08-02 18:06:42 +0430

    add 404 & 500 Route

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,15 @@ let index_controller = require('../controllers/indexController')
 router.use('/api', require('./api'));
 router.route('/').get(index_controller.home);
 
+// 404 Route
+router.use(function (req, res, next) {
+    res.status(404).render('errors/404');
+})
+
+// 500 Inrernal Server Error Route
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
 module.exports = router
